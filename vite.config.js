b9dab7acc2011f2e4da3546875521462394e2b62
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-01-16 19:20:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/dandadan-startpage/",
-  root: "./src"
-})
+  base: "/dandadan-startpage/", // Base path for GitHub Pages
+  root: "./src", // Set the root directory
+
+  server: {
+    headers: {
+      // Add Content Security Policy headers to allow GitHub Pages scripts
+      "Content-Security-Policy":
+        "script-src 'self' https://gopicharanreddy.github.io; object-src 'none';",
+    },
+  },
+
+  build: {
+    outDir: "../dist", // Adjust output directory relative to the root
+    emptyOutDir: true, // Clean the output directory before building
+  },
+});
